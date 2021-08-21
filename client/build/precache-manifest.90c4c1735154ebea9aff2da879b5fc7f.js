033c70d7ae1a03932844b06086c38a26da794e5f
Mukund Kedia <mukundkedia7478@gmail.com>
2021-08-22 00:28:48 +0530

    committed

---
+++ client/build/precache-manifest.90c4c1735154ebea9aff2da879b5fc7f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0233d2699ad918427686d632143b009a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "eb1487113f5998540112",
+    "url": "/static/css/main.6ebd63e0.chunk.css"
+  },
+  {
+    "revision": "ac7a5c3e32cd02e84a22",
+    "url": "/static/js/2.c166ff5e.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.c166ff5e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eb1487113f5998540112",
+    "url": "/static/js/main.ddbe3d3c.chunk.js"
+  },
+  {
+    "revision": "00fc4afc96365658fc09",
+    "url": "/static/js/runtime-main.eda899bb.js"
+  }
+]);
